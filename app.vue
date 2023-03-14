ad76e1fb834661c3841694654291b5425cf24e12
kangpeter5 <kangpeter5@gmail.com>
2023-03-14 15:38:44 -0700

    basic page setup

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <header class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md">
+      <a href="" class="text-3xl font-mono">cartrader</a>
+    </header>
+
   </div>
 </template>
